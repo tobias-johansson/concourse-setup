8406179aab5e0eeeeb35bae7a2a06efae3f2237b
Tobias Johansson <tobias.johansson@seal-software.com>
2016-04-25 17:52:41 +0200

    progress

--- tasks/add-pipelines.sh
+++ tasks/add-pipelines.sh
@@ -7,6 +7,7 @@ set -x
 fly -t ci login -c http://192.168.100.4:8080/
 for file in concourse-setup/pipelines/*.yml; do
   name="$(basename "$file")"
+  name=${name%.yml}
   echo $file
   echo $name
 done
